acd9569f95c8dbb5f3d749c34cd6d305ec3791b6
Qiwen Zeng <qzeng7676@gmail.com>
2024-02-23 17:22:48 -0800

    pug updated

--- public/js/login.js
+++ public/js/login.js
@@ -31,7 +31,9 @@ export const logout = async () => {
       method: 'GET',
       url: 'http://127.0.0.1:8000/api/v1/users/logout'
     });
-    if ((res.data.status = 'success')) location.reload(true);
+    if ((res.data.status = 'success')) {
+      location.reload(true);
+    }
   } catch (err) {
     console.log(err.response);
     showAlert('error', 'Error logging out! Try again.');
